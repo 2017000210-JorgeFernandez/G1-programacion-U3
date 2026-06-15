ca4afe704918b23ce7468a4b4d8578c0df95536c
2017000210-JorgeFernandez <j.fernandez.5bdb@gmail.com>
2026-06-14 22:39:38 -0600

    Add files via upload

---
+++ Lección 16 actividad 2 ejercicio 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+
+static void Main()
+
+intl edadesPersonas = new int[7];
+int cantidadMayoresEdad = 0;
+
+for (int i = 0; i < 7; i++)
+
+    Console.Write($'"Ingrese la edad de la persona {i
+    + 1}: ");
+edadesPersonas[i] =
+int.Parse(Console.ReadLine());
+
+if (edadesPersonas[i] >= 18)
+
+    cantidadMayoresEdad++;
+
+Console.WriteLine('$"InCantidad de personas
+
+mayores de edad: { cantidadMayoresEdad}
+");
+}
