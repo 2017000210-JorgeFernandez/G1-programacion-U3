a39976b08eba98ec2f92cc5ab3ab80f2ed5bdcaa
2017000210-JorgeFernandez <j.fernandez.5bdb@gmail.com>
2026-06-14 22:52:18 -0600

    Add files via upload

---
+++ Leccion 16 actividad 3 ejercicio 1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+
+static void Main()
+
+int[numerosEnteros = new int[15];
+bool existeRepetido = false;
+
+// Ingreso de datos
+for (int indice = 0; indice < 15; indice++)
+
+    Console.Write($"Ingrese el número {indice + 1}:
+    ");
+    numerosEnteros[indice] =
+    int.Parse(Console.ReadLine());
+
+// Buscar números repetidos
+for (int indice = 0; indice < 14; indice++)
+
+    for (int comparador = indice + 1; comparador <
+
+    15; comparador++)
+        if (numerosEnteros[indice] ==
+numerosEnteros[comparador])
+
+            existeRepetido = true;
+
+// Mostrar resultado
+if (existeRepetido)
+
+    Console.WriteLine("Sí existe al menos un
+    número repetido.");
+
+else
+
+    Console.WriteLine("No existe ningún número
+    repetido.");
+
+}
+
---
+++ leccion 16 act 2 eje2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+
+static void Main()
+
+int[numerosEnteros = new int[8];
+int numeroMayor = 0;
+int posicionMayor = 0;
+
+for (int i = 0; i < 8; i++)
+
+    Console.Write($'"Ingrese el número {i + 1}:");
+    numerosEnteros[i] =
+    int.Parse(Console.ReadLine());
+
+if (i == 0)
+
+    numeroMayor = numerosEnteros[i];
+posicionMayor = i + 1;
+
+else if (numerosEnteros[i] > numeroMayor)
+
+    -
+
+    numeroMayor = numerosEnteros[i];
+posicionMayor = i + 1;
+
+Console.WriteLine($"\nEl número mayor es:
+{ numeroMayor}
+");
+Console.WriteLine($"Se encuentra en la
+posición: { posicionMayor}
+");
